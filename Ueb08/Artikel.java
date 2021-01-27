f86e4d368bfb75adab0153a5df67d95cc38aa4c7
simon <Kapalatus@web.de>
2021-01-27 18:03:44 +0100

    fomrat

--- Ueb08/Artikel.java
+++ Ueb08/Artikel.java
@@ -4,25 +4,24 @@ public class Artikel {
     private int bestand;
     private double preis;
 
-
-    public Artikel(){
+    public Artikel() {
     };
 
-    public Artikel(int artikelNr, String art){
+    public Artikel(int artikelNr, String art) {
         this(artikelNr, art, 0);
     }
 
-    public Artikel(int artikelNr, String art, int bestand){
+    public Artikel(int artikelNr, String art, int bestand) {
         this(artikelNr, art, bestand, 0);
     }
 
-    public Artikel(int artikelNr, String art, int bestand, double preis){
-        if (artikelNr < 10000 && artikelNr > 999){
+    public Artikel(int artikelNr, String art, int bestand, double preis) {
+        if (artikelNr < 10000 && artikelNr > 999) {
             this.artikelNr = artikelNr;
         } else {
             throw new IllegalStateException("Artikelnummer ist nicht vierstellig");
         }
-        if (art != null){
+        if (art != null) {
             this.art = art;
         } else {
             throw new IllegalStateException("Die Bezeichnung ist null");
@@ -30,65 +29,65 @@ public class Artikel {
         setBestand(bestand);
 
         setPreis(preis);
-        
+
     }
-    
-    public void bucheAbgang(int menge){
-        if(menge < 0){
+
+    public void bucheAbgang(int menge) {
+        if (menge < 0) {
             throw new IllegalStateException("Menge ist kleiner 0");
         }
         setBestand(bestand -= menge);
     }
 
-    public void bucheZugang(int menge){
-        if(menge >= 0){
+    public void bucheZugang(int menge) {
+        if (menge >= 0) {
             setBestand(bestand += menge);
         } else {
             throw new IllegalStateException("Menge ist kleiner 0");
         }
     }
 
-    public int getArtikelNr(){
+    public int getArtikelNr() {
         return artikelNr;
     }
-    
-    public String getBezeichnung(){
+
+    public String getBezeichnung() {
         return art;
     }
 
-    public int getBestand(){
+    public int getBestand() {
         return bestand;
     }
-    
-    public double getPreis(){
+
+    public double getPreis() {
         return preis;
     }
 
-    public void setBestand(int bestandNeu){
-        if(bestand >= 0){
+    public void setBestand(int bestandNeu) {
+        if (bestand >= 0) {
             bestand = bestandNeu;
         } else {
             throw new IllegalStateException("Bestand ist kleiner 0");
         }
     }
 
-    public void setArt(String art){
-        if ( art == null){
+    public void setArt(String art) {
+        if (art == null) {
             throw new IllegalStateException("Art ist null");
         }
         this.art = art.trim();
     }
-    
-    public void setPreis(double neuerPreis){
-        if (preis <= 0){
+
+    public void setPreis(double neuerPreis) {
+        if (preis <= 0) {
             throw new IllegalStateException("Preis ist kleiner oder 0");
         } else {
             this.preis = Math.round(preis * 100) / 100;
         }
     }
 
-    public void aenderePreis(double prozent){
-        if(prozent == 0.0){
+    public void aenderePreis(double prozent) {
+        if (prozent == 0.0) {
             throw new IllegalStateException("Preisänderung ist 0%");
         } else {
             preis = preis * prozent;
@@ -96,11 +95,12 @@ public class Artikel {
     }
 
     @Override
-    public String toString(){
-        return "Artikelnummer: " + artikelNr + "   Bezeichnung: " + art + "   Bestand: " + bestand + "  Preis: " + preis;
+    public String toString() {
+        return "Artikelnummer: " + artikelNr + "   Bezeichnung: " + art + "   Bestand: " + bestand + "  Preis: "
+                + preis;
     }
 
-    public String getBeschreibung(){
+    public String getBeschreibung() {
         return art;
     }
 
